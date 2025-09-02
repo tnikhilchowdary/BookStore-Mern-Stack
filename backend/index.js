3c5f88f7df25d0747197843f59e88340eacd79fb
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-09-02 13:06:54 -0500

    PUT Method

--- backend/index.js
+++ backend/index.js
@@ -66,6 +66,29 @@ app.get("/book/:id", async (req, res) => {
 })
 
 
+app.put('/books/:id', async (req, res) => {
+    try{
+        if(!req.body.title || !req.body.author || !req.body.publishYear){
+            return res.status(400).send({
+                message:"Send all required fields:title, author, publishYear",
+            });
+        }
+        const {id} = req.params;
+        const result = await Book.findByIdAndUpdate(id, req.body);
+        if(!result){
+            return res.status(404).json({message: "Book not Found"})
+        }
+
+        return res.status(200).send({message:'Book Updated Succssfully'})
+    }
+    catch(error){
+        console.log(error.message);
+        res.status(500).send({message:error.message});
+    }
+});
+
+
+
 app.listen(PORT, () => {
     console.log(`App is listening to PORT: ${PORT}`);
 });
