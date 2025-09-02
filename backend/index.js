dbaf11d7675f38776e039836b397180640dd2cca
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-09-02 16:40:28 -0500

    Delete Crud Operations

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { response } from "express"
 import mongoose from "mongoose";
 import {PORT, mongoDBURL} from "./config.js";
 import {Book} from "./models/bookModel.js";
@@ -84,6 +84,24 @@ app.put('/books/:id', async (req, res) => {
     catch(error){
         console.log(error.message);
         res.status(500).send({message:error.message});
+    }
+});
+
+
+app.delete('/booksdelete/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        return res.status(200).json({ message: "Book deleted successfully" });
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
     }
 });
 
